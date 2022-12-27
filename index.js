049856fea0cb4fdb1a67c53a8d99c01e9d948cde
sebastianbarrozo <sebastian.barrozo@etendo.software>
2022-12-27 15:13:44 -0300

    Feature EE-459: Fixed iOS app

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-import { AppRegistry } from "react-native";
-import { decode } from "base-64";
-import * as Sentry from "@sentry/react-native";
-import App from "./App";
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-// Sentry.init({
-//   dsn:
-//     "https://e89769bc69164ad19c89bceac42ecfc8@o391053.ingest.sentry.io/5661649"
-// });
 
 AppRegistry.registerComponent(appName, () => App);
