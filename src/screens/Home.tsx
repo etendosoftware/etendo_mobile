4684b63b3fe4e3132efad0bb0226fb1dc15c9b23
Leandro Allemandi <leandro.allemandi@smfconsulting.es>
2023-01-30 11:10:43 -0300

    Feature EPL-472: Formater home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,7 +6,7 @@ import {
   View,
   Text,
   TouchableOpacity,
-  Platform
+  Platform,
 } from "react-native";
 import locale from "../i18n/locale";
 import withAuthentication from "../withAuthentication";
@@ -16,9 +16,9 @@ import MainAppContext from "../contexts/MainAppContext";
 import { INavigation } from "../components/Card";
 import Icon from "react-native-vector-icons/Ionicons";
 import { defaultTheme } from "../themes";
-import { isTablet } from '../../hook/isTablet';
+import { isTablet } from "../../hook/isTablet";
 
-const etendoBoyImg = require("../img/etendo_boy_back.png")
+const etendoBoyImg = require("../img/etendo_boy_back.png");
 import { useNavigation } from "@react-navigation/native";
 import { Etendo } from "../helpers/Etendo";
 
@@ -49,21 +49,18 @@ class HomeClass extends React.Component<Props, State> {
         <View style={styles.conteinerSup}>
           <View
             style={{
-
-              flex:1,
-              backgroundColor: defaultTheme.colors.accent
-            }}
-          >
+              flex: 1,
+              backgroundColor: defaultTheme.colors.accent,
+            }}>
             <Image
               style={styles.logo}
-              resizeMode={'stretch'}
+              resizeMode={"stretch"}
               source={require("../img/home2.png")}
             />
           </View>
           <View style={styles.etendoContainer}>
-          <Image
+            <Image
               style={styles.etendo}
-           
               source={require("../img/etendo-logo-1.png")}
             />
             <Text allowFontScaling={false} style={styles.text}>
@@ -74,7 +71,7 @@ class HomeClass extends React.Component<Props, State> {
             style={{
               width: "10%",
               backgroundColor: defaultTheme.colors.accent,
-              height: "100%"
+              height: "100%",
             }}
           />
         </View>
@@ -83,8 +80,7 @@ class HomeClass extends React.Component<Props, State> {
             <Icon name="person-circle" size={25} style={styles.TextIcon} />
             <TouchableOpacity
               activeOpacity={0.6}
-              onPress={() => this.props.navigation.navigate("Profile")}
-            >
+              onPress={() => this.props.navigation.navigate("Profile")}>
               <Button>
                 <Text allowFontScaling={false} style={styles.TextIcon}>
                   {locale.t("Profile")}{" "}
@@ -96,8 +92,7 @@ class HomeClass extends React.Component<Props, State> {
             <Icon name="md-settings" size={20} style={styles.TextIcon} />
             <TouchableOpacity
               activeOpacity={0.6}
-              onPress={() => this.props.navigation.navigate("Settings")}
-            >
+              onPress={() => this.props.navigation.navigate("Settings")}>
               <Button>
                 <Text allowFontScaling={false} style={styles.TextIcon}>
                   {locale.t("Settings")}
@@ -105,11 +100,8 @@ class HomeClass extends React.Component<Props, State> {
               </Button>
             </TouchableOpacity>
           </View>
-        </View> 
-        <Image
-            style={styles.image}
-            source={etendoBoyImg}
-          />
+        </View>
+        <Image style={styles.image} source={etendoBoyImg} />
       </View>
     );
   }
@@ -118,61 +110,58 @@ const Home = (props) => {
   const navigation = useNavigation();
   Etendo.globalNav = navigation;
 
-  return (
-    <HomeClass {...props} />
-  )
-}
+  return <HomeClass {...props} />;
+};
 export default withAuthentication(Home);
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     backgroundColor: defaultTheme.colors.background,
-    height: '100%'
+    height: "100%",
   },
   image: {
     height: 342,
     width: 364,
-    right:0,
-    bottom:0,
-    position: "absolute"
+    right: 0,
+    bottom: 0,
+    position: "absolute",
   },
   logo: {
     height: "100%",
     width: 130,
-  
   },
   etendo: {
     height: 50,
     width: 200,
   },
   etendoContainer: {
-    height: "100%", 
+    height: "100%",
     width: isTablet() ? 260 : 220,
-    alignItems:'center',
-    justifyContent:'center', 
-    flexDirection:'column'
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
   },
   text: {
     color: defaultTheme.colors.textSecondary,
     fontSize: 20,
-    alignSelf:'flex-end',
-    paddingRight:  isTablet() ? 40 : 20,
+    alignSelf: "flex-end",
+    paddingRight: isTablet() ? 40 : 20,
   },
 
   conteinerSup: {
-    width: '100%',
+    width: "100%",
     flexDirection: "row",
     justifyContent: "space-between",
     height: 80,
     alignItems: "center",
-    marginTop: 20
+    marginTop: 20,
   },
   conteinerMed: {
     display: "flex",
     width: "100%",
     flexDirection: "row",
     justifyContent: "center",
-    alignItems:'center',
+    alignItems: "center",
     height: "8%",
     marginTop: 20,
   },
@@ -180,14 +169,14 @@ const styles = StyleSheet.create({
     display: "flex",
     width: "100%",
     flexDirection: "row",
-    height: "80%"
+    height: "80%",
   },
   button: {
-    width: '50%',
+    width: "50%",
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    justifyContent:'center'
+    justifyContent: "center",
   },
   buttonFaq: {
     width: "100%",
@@ -196,9 +185,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     position: "relative",
     marginLeft: "62%",
-    marginTop: Platform.OS === "ios" ? "10%" : "25%"
+    marginTop: Platform.OS === "ios" ? "10%" : "25%",
   },
   TextIcon: {
-    color: defaultTheme.colors.textSecondary
-  }
+    color: defaultTheme.colors.textSecondary,
+  },
 });
