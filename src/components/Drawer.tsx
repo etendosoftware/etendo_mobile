6cb7badbc0ea36b82a8c93ea5d92eb927ee6917f
Santiago Cuello <sjcuello@gmail.com>
2023-06-06 12:29:27 -0300

    Fix theme and Drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,14 +1,6 @@
-import React, { useContext } from "react";
-import {
-  View,
-  Image,
-  Dimensions,
-  StyleSheet,
-  Text,
-  Pressable
-} from "react-native";
+import React, { useContext, useState, useEffect } from "react";
+import { View, Image, StyleSheet, Text, Pressable } from "react-native";
 import { User, Windows, logout } from "../stores";
-import { DefaultTheme } from "react-native-paper";
 import { observer } from "mobx-react";
 import { OBRest, Restrictions } from "obrest";
 import { IRecord } from "../types/Record";
@@ -20,8 +12,6 @@ import { defaultTheme } from "../themes";
 import ShowProfilePicture from "./ShowProfilePicture";
 import { ContainerContext } from "../contexts/ContainerContext";
 import { Etendo } from "../helpers/Etendo";
-
-const NOAUTH_SCREENS = ["Settings", "Login"];
 
 interface IRoute {
   name: string;
@@ -39,184 +29,160 @@ interface Props {
   onItemPress: object;
   getLabel: object;
   routes?: IRoute[];
-  // navigation: INavigation;
+  navigation: {
+    navigate: (route: string, params?: object) => void; // Assuming navigate accepts a string route and an optional object parameter, adjust if necessary
+  };
+  context: any,
   state: {
     routes: IRoute[];
   };
 }
 
-interface State {
-  menuItems: IRoute[];
-  loadingMenu: boolean;
-  userName: string;
-  organization: string;
-}
+const DrawerClass = observer((props: Props) => {
+  const [menuItems, setMenuItems] = useState([]);
+  const [loadingMenu, setLoadingMenu] = useState(true);
+  const [userName, setUserName] = useState("");
+  const [organization, setOrganization] = useState("");
 
-const win = Dimensions.get("window");
-const ratio = win.width / 1080; //541 is actual image width
-
-@observer
-export class DrawerClass extends React.Component<Props, State> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menuItems: [],
-      loadingMenu: true,
-      userName: "",
-      organization: ""
-    };
-    observe(Windows, (change) => {
-      let loading = true;
-      if (Windows.menuItems.length > 0) {
-        loading = false;
-      }
-      if (this.state.loadingMenu !== loading) {
-        this.setState({ loadingMenu: loading });
-      }
+  useEffect(() => {
+    const disposer = observe(Windows, (change) => {
+      setLoadingMenu(!(Windows.menuItems.length > 0));
     });
-  }
-  componentDidUpdate = (_, prevState) => {
-    let newMenuItems = Windows.menuItems;
+
+    return () => disposer(); // cleanup observer when component unmounts
+  }, []);
+
+  useEffect(() => {
+    const fetchOrganizationName = async () => {
+      const organization = await getOrganizationName();
+      setOrganization(organization);
+      setLoadingMenu(menuItems.length === 0);
+    };
+
+    fetchOrganizationName();
+  }, []); // run once after first render
+
+  useEffect(() => {
     if (
       !Windows.loading &&
       !User.loading &&
       User.token &&
-      prevState.menuItems != newMenuItems
+      menuItems !== Windows.menuItems
     ) {
-      // not loading but logged in, and menus are not loaded
-      this.setState({ menuItems: newMenuItems, userName: User.data.username });
+      setMenuItems(Windows.menuItems);
+      setUserName(User.data.username);
     }
-  };
-  componentDidMount = async () => {
-    const organization = await this.getOrganizationName();
-    this.setState({ organization });
-    if (this.state.menuItems === []) {
-      this.setState({ loadingMenu: true });
-    } else {
-      this.setState({ loadingMenu: false });
-    }
-  };
+  }, [Windows, User, menuItems]); // run whenever Windows, User, or menuItems changes
 
-  handleLogout = async () => {
+  const handleLogout = async () => {
     await logout();
-    this.setState({ menuItems: [] });
+    setMenuItems([]);
   };
 
-  getInitials = function(string) {
-    var names = string.split(" "),
-      initials = names[0].substring(0, 1).toUpperCase();
-    if (names.length > 1) {
-      initials += names[names.length - 1].substring(0, 1).toUpperCase();
-    }
-    return initials;
-  };
-
-  getOrganizationName = async () => {
+  const getOrganizationName = async () => {
     let criteria = OBRest.getInstance().createCriteria("Organization");
     criteria.add(Restrictions.equals("id", User.data.organization));
     var org: IRecord = await criteria.uniqueResult();
     return org.name;
   };
 
-  render() {
-    if (Windows.loading) {
-      return null;
-    }
-    var focused = false;
-    return (
-      <DrawerContentScrollView {...this.props}>
-        <View>
-          <Image
-            style={styles.image}
-            source={require("../../src/img/drawer.png")}
-          />
-          <View style={styles.topData}>
-            <Pressable
-              style={styles.pressableStyle}
-              onPress={() => this.props.navigation.navigate("Profile")}
-            >
-              <ShowProfilePicture
-                size={85}
-                username={this.state.userName}
-              ></ShowProfilePicture>
-            </Pressable>
-            <View style={styles.dataProfile}>
-              <Text allowFontScaling={false} style={styles.textProfile}>
-                {this.state.userName}
-              </Text>
-              <Text allowFontScaling={false} style={styles.textProfile}>
-                {this.state.organization}
-              </Text>
-            </View>
+  if (Windows.loading || !loadingMenu) {
+    return null;
+  }
+  var focused = false;
+  return (
+    <DrawerContentScrollView {...props}>
+      <View>
+        <Image
+          style={styles.image}
+          source={require("../../src/img/drawer.png")}
+        />
+        <View style={styles.topData}>
+          <Pressable
+            style={styles.pressableStyle}
+            onPress={() => props.navigation.navigate("Profile")}
+          >
+            <ShowProfilePicture
+              size={85}
+              username={userName}
+            ></ShowProfilePicture>
+          </Pressable>
+          <View style={styles.dataProfile}>
+            <Text allowFontScaling={false} style={styles.textProfile}>
+              {userName}
+            </Text>
+            <Text allowFontScaling={false} style={styles.textProfile}>
+              {organization}
+            </Text>
           </View>
         </View>
-        <View style={styles.constantItemsSecondary}>
-          <Icon name="md-home" size={20} style={styles.iconColor} />
-          <DrawerItem
-            activeTintColor={DefaultTheme.colors.textSecondary}
-            inactiveTintColor={DefaultTheme.colors.textSecondary}
-            style={styles.drawerItem}
-            key={0}
-            labelStyle={styles.drawerText}
-            label={locale.t("Home")}
-            onPress={() => this.props.navigation.navigate("Home")}
-          />
-        </View>
+      </View>
+      <View style={styles.constantItemsSecondary}>
+        <Icon name="md-home" size={20} style={styles.iconColor} />
+        <DrawerItem
+          activeTintColor={defaultTheme.colors.textSecondary}
+          inactiveTintColor={defaultTheme.colors.textSecondary}
+          style={styles.drawerItem}
+          key={0}
+          labelStyle={styles.drawerText}
+          label={locale.t("Home")}
+          onPress={() => props.navigation.navigate("Home")}
+        />
+      </View>
 
-        {this.props.context?.state?.menuItems.map((menuItem: any) => {
-          const params = { ...menuItem };
-          if (params.component) {
-            delete params.component;
-          }
-          return (
-            <DrawerItem
-              activeTintColor={"black"}
-              inactiveTintColor={"black"}
-              key={menuItem.__id}
-              label={menuItem.name}
-              labelStyle={styles.drawerText}
-              style={styles.drawerItem}
-              onPress={async () => {
-                if (menuItem.app) {
-                  Etendo.navigation[menuItem.app].navigate(
-                    menuItem.route,
-                    menuItem
-                  );
-                  Etendo.toggleDrawer();
-                } else {
-                  Etendo.globalNav.navigate(menuItem.name, menuItem.params);
-                }
-              }}
-            />
-          );
-        })}
-        <View style={styles.constantItems}></View>
-        <View style={styles.drawerItemsContainer}>
-          <Icon name="md-settings" size={20} style={styles.iconColor} />
+      {props.context?.state?.menuItems.map((menuItem: any) => {
+        const params = { ...menuItem };
+        if (params.component) {
+          delete params.component;
+        }
+        return (
           <DrawerItem
-            activeTintColor={DefaultTheme.colors.textSecondary}
-            inactiveTintColor={DefaultTheme.colors.textSecondary}
+            activeTintColor={"black"}
+            inactiveTintColor={"black"}
+            key={menuItem.__id}
+            label={menuItem.name}
             labelStyle={styles.drawerText}
             style={styles.drawerItem}
-            label={locale.t("Settings")}
-            onPress={() => this.props.navigation.navigate("Settings")}
+            onPress={async () => {
+              if (menuItem.app) {
+                Etendo.navigation[menuItem.app].navigate(
+                  menuItem.route,
+                  menuItem
+                );
+                Etendo.toggleDrawer();
+              } else {
+                Etendo.globalNav.navigate(menuItem.name, menuItem.params);
+              }
+            }}
           />
-        </View>
-        <View style={styles.drawerItemsContainer}>
-          <Icon name="md-log-out" size={20} style={styles.iconColor} />
-          <DrawerItem
-            activeTintColor={DefaultTheme.colors.textSecondary}
-            inactiveTintColor={DefaultTheme.colors.textSecondary}
-            style={styles.drawerItem}
-            label={locale.t("Log out")}
-            labelStyle={styles.drawerText}
-            onPress={this.handleLogout}
-          />
-        </View>
-      </DrawerContentScrollView>
-    );
-  }
-}
+        );
+      })}
+      <View style={styles.constantItems}></View>
+      <View style={styles.drawerItemsContainer}>
+        <Icon name="md-settings" size={20} style={styles.iconColor} />
+        <DrawerItem
+          activeTintColor={defaultTheme.colors.textSecondary}
+          inactiveTintColor={defaultTheme.colors.textSecondary}
+          labelStyle={styles.drawerText}
+          style={styles.drawerItem}
+          label={locale.t("Settings")}
+          onPress={() => props.navigation.navigate("Settings")}
+        />
+      </View>
+      <View style={styles.drawerItemsContainer}>
+        <Icon name="md-log-out" size={20} style={styles.iconColor} />
+        <DrawerItem
+          activeTintColor={defaultTheme.colors.textSecondary}
+          inactiveTintColor={defaultTheme.colors.textSecondary}
+          style={styles.drawerItem}
+          label={locale.t("Log out")}
+          labelStyle={styles.drawerText}
+          onPress={handleLogout}
+        />
+      </View>
+    </DrawerContentScrollView>
+  );
+});
 
 export const Drawer = (props) => {
   const context = useContext(ContainerContext);
