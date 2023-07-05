6d5bb63b2a75d117be891239caef0b535a407aa8
Santiago Cuello <santiago.cuello@smfconsulting.es>
2023-07-05 12:49:13 -0300

    Feature EPL-757: Update post clone

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,9 +2,6 @@
 
 # Add authentication to our private repo to install npm packages
 echo """
-registry=https://repo.futit.cloud/repository/npm-group/
-always-auth=true
-_auth=${NPM_TOKEN}
 """ > .npmrc
 
 exit 0
