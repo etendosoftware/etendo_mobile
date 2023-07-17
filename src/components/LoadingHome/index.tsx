e69ba5ad3acb7667ed64b241ded8fd5c0916acf6
Leandro Allemandi <leandro.allemandi@smfconsulting.es>
2023-07-17 11:51:48 -0300

    Feature EPL-764: add for in loadingHome

--- src/components/LoadingHome/index.tsx
+++ src/components/LoadingHome/index.tsx
@@ -1,34 +1,22 @@
-import { View } from "react-native";
 import React from "react";
 import CardDropdown from "etendo-ui-library/dist-native/components/cards/cardDropdown/CardDropdown";
+import { View } from "react-native";
 import { styles } from "./styles";
 
 const LoadingHome = () => {
-  return (
-    <View style={styles.container}>
-      <View>
+  const numberOfCards = 4;
+
+  const cardComponents = [];
+
+  for (let i = 0; i < numberOfCards; i++) {
+    cardComponents.push(
+      <View style={i !== 0 && styles.cardContainer} key={i}>
         <CardDropdown title={""} />
       </View>
-      <View style={styles.cardContainer}>
-        <CardDropdown title={""} />
-      </View>
-      <View style={styles.cardContainer}>
-        <CardDropdown title={""} />
-      </View>
-      <View style={styles.cardContainer}>
-        <CardDropdown title={""} />
-      </View>
-      <View style={styles.cardContainer}>
-        <CardDropdown title={""} />
-      </View>
-      <View style={styles.cardContainer}>
-        <CardDropdown title={""} />
-      </View>
-      <View style={styles.cardContainer}>
-        <CardDropdown title={""} />
-      </View>
-    </View>
-  );
+    );
+  }
+
+  return <View style={styles.container}>{cardComponents}</View>;
 };
 
 export default LoadingHome;
