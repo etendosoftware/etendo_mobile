f3e3757666e31672c7812590df82d4fe84a3de50
Leandro Allemandi <leandro.allemandi@smfconsulting.es>
2023-07-18 12:35:03 -0300

    Feature EPL-764: change SET_BINDARY_IMG text

--- src/contexts/actionsTypes.tsx
+++ src/contexts/actionsTypes.tsx
@@ -1,3 +1,3 @@
 export const SET_URL = "SET_URL";
 export const SET_LOADING = "SET_LOADING";
-export const SET_BINDARY_IMG = "SET_BINDARY_PROFILE_IMG";
+export const SET_BINDARY_IMG = "SET_BINARY_IMG";
