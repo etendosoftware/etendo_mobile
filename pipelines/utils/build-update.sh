90324d6c94d247cc7503bbe011411787f4aa6d25
Valeria Garcia <valeria.garcia@smfconsulting.es>
2024-08-14 12:20:36 -0300

    Feature ETP-6: Migrate pipeline to Github

--- pipelines/utils/build-update.sh
+++ pipelines/utils/build-update.sh
@@ -2,17 +2,25 @@
 
 # Parameters
 REPO_SLUG=$1
-REVISION=$2
-BUILD_ID=$3
-STATE=$4
-JOB_NAME=$5
-BUILD_URL=$6
-DESCRIPTION=$7
-OWNER=$8
-USER=$9
-TOKEN=${10}
+STATUS=$2
+DESCRIPTION=$3
+ACCESS_TOKEN=$4
+REVISION=$5
+TARGET_URL="$6"
+CONTEXT="$7"
 
-# Function to display error message and exit
+# GitHub API URL
+URI='api.github.com'
+OWNER='etendosoftware'
+
+# GitHub status URL
+GIT_STATUS_URL="https://$ACCESS_TOKEN:x-oauth-basic@$URI/repos/$OWNER/$REPO_SLUG/statuses/${REVISION}"
+
+# Template for JSON data
+TEMPLATE='{"state":"%s", "target_url":"%s", "description":"%s", "context":"%s"}'
+PAYLOAD=$(printf "$TEMPLATE" "$STATUS" "$TARGET_URL" "$DESCRIPTION" "$CONTEXT")
+
+# Function to display error messages and exit
 display_error() {
   echo "***********************"
   echo "BUILD-UPDATE Error"
@@ -20,65 +28,46 @@ display_error() {
   echo "***********************"
   echo "Parameters received:"
   echo "REPO_SLUG: ${REPO_SLUG:-EMPTY}"
-  echo "REVISION: ${REVISION:-EMPTY}"
-  echo "BUILD_ID: ${BUILD_ID:-EMPTY}"
-  echo "STATE: ${STATE:-EMPTY}"
-  echo "JOB_NAME: ${JOB_NAME:-EMPTY}"
-  echo "BUILD_URL: ${BUILD_URL:-EMPTY}"
+  echo "STATUS: ${STATUS:-EMPTY}"
   echo "DESCRIPTION: ${DESCRIPTION:-EMPTY}"
-  echo "OWNER: ${OWNER:-EMPTY}"
-  echo "USER: ${USER:-EMPTY}"
-  echo "TOKEN: ${TOKEN:-EMPTY}"
+  echo "ACCESS_TOKEN: ${ACCESS_TOKEN:-EMPTY}"
+  echo "REVISION: ${REVISION:-EMPTY}"
+  echo "TARGET_URL: ${TARGET_URL:-EMPTY}"
+  echo "CONTEXT: ${CONTEXT:-EMPTY}"
   echo "***********************"
   exit 1
 }
 
 # Check for empty parameters and exit with an error message if any are found
 if [ -z "$REPO_SLUG" ]; then display_error "REPO_SLUG is empty"; fi
-if [ -z "$REVISION" ]; then display_error "REVISION is empty"; fi
-if [ -z "$BUILD_ID" ]; then display_error "BUILD_ID is empty"; fi
-if [ -z "$STATE" ]; then display_error "STATE is empty"; fi
-if [ -z "$JOB_NAME" ]; then display_error "JOB_NAME is empty"; fi
-if [ -z "$BUILD_URL" ]; then display_error "BUILD_URL is empty"; fi
+if [ -z "$STATUS" ]; then display_error "STATUS is empty"; fi
 if [ -z "$DESCRIPTION" ]; then display_error "DESCRIPTION is empty"; fi
-if [ -z "$OWNER" ]; then display_error "OWNER is empty"; fi
-if [ -z "$USER" ]; then display_error "USER is empty"; fi
-if [ -z "$TOKEN" ]; then display_error "TOKEN is empty"; fi
+if [ -z "$ACCESS_TOKEN" ]; then display_error "ACCESS_TOKEN is empty"; fi
+if [ -z "$REVISION" ]; then display_error "REVISION is empty"; fi
+if [ -z "$TARGET_URL" ]; then display_error "TARGET_URL is empty"; fi
+if [ -z "$CONTEXT" ]; then display_error "CONTEXT is empty"; fi
 
-# Template for JSON data
-template='{"key": "%s", "state": "%s", "name": "%s", "url": "%s", "description": "%s"}'
+# Print the URL and data for debugging purposes
+echo "$PAYLOAD"
+echo "$GIT_STATUS_URL"
 
-# Formatted JSON data
-DATA=$(printf "$template" "$BUILD_ID" "$STATE" "$JOB_NAME #$BUILD_ID" "$BUILD_URL" "$DESCRIPTION")
-
-# Bitbucket API URL
-URI='https://api.bitbucket.org/2.0/repositories'
-URL="$URI/$OWNER/$REPO_SLUG/commit/$REVISION/statuses/build"
-
-# Print URL and DATA for debugging purposes
-echo "$URL"
-echo "$DATA"
-
-# cURL request to update the build status on Bitbucket
-RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -u "$USER:$TOKEN" "$URL" --header "Content-Type: application/json" --data "$DATA")
+# Perform the cURL request to update the build status on GitHub
+RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "application/json" -d "$PAYLOAD" "$GIT_STATUS_URL")
 
 # Check if the response code is not 20x
 if [[ ! "$RESPONSE" =~ ^20[0-9]$ ]]; then
   echo "***********************"
   echo "BUILD-UPDATE Error"
-  echo "Failed to update build status on Bitbucket. HTTP status code: $RESPONSE"
+  echo "Failed to update build status on GitHub. HTTP status code: $RESPONSE"
   echo "***********************"
   echo "Parameters received:"
   echo "REPO_SLUG: $REPO_SLUG"
-  echo "REVISION: $REVISION"
-  echo "BUILD_ID: $BUILD_ID"
-  echo "STATE: $STATE"
-  echo "JOB_NAME: $JOB_NAME"
-  echo "BUILD_URL: $BUILD_URL"
+  echo "STATUS: $STATUS"
   echo "DESCRIPTION: $DESCRIPTION"
-  echo "OWNER: $OWNER"
-  echo "USER: $USER"
-  echo "TOKEN: $TOKEN"
+  echo "ACCESS_TOKEN: $ACCESS_TOKEN"
+  echo "REVISION: $REVISION"
+  echo "TARGET_URL: $TARGET_URL"
+  echo "CONTEXT: $CONTEXT"
   echo "***********************"
   exit 1
 fi
